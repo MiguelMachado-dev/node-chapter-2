40873320fb3a37dc6263f02862e6efa0611f0cb1
Miguel Machado <miiguellmp@gmail.com>
2021-07-31 10:29:52 -0300

    feat(debugger): set debugger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
 const port = process.env.PORT || 3333;
 
 app.get("/", (req, res) => {
   return res.json({ message: "Hello World"});
+})
+
+app.post("/courses", (req, res) => {
+  const { name } = req.body;
+
+  return res.json({ name });
 })
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
