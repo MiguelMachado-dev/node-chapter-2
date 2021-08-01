63fadf3c9d7f389a94db64aa2c8b508ffd6be6ab
Miguel Machado <miiguellmp@gmail.com>
2021-08-01 15:36:09 -0300

    feat(repository): create category repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,27 +1,16 @@
 import { Router } from 'express'
 
-import { Category } from '../model/Category'
+import { CategoriesRepository } from '../repositories/CategoriesRepository'
 
 const categoriesRoutes = Router()
-
-const categories: Array<Category> = []
+const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body
 
-  const category = new Category()
-
-  Object.assign(category, { name, description, created_at: new Date() })
-
-  categories.push(category)
+  categoriesRepository.create({ name, description })
 
   return res.status(201).send()
-})
-
-// o path para essa rota será /categories/list pois no server
-// inserimos o base path como /categories direto na chamada do middleware
-categoriesRoutes.get('/list', (req, res) => {
-  return res.json(categories).status(200)
 })
 
 export { categoriesRoutes }
